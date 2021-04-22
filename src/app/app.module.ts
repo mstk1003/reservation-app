8d65ca4c488b39a0b70549ea0f64a317810f1857
Sato Masataka <masataka@SatonoMacBook-Air.local>
2021-04-22 11:37:55 +0900

    商品リストと詳細を実装。SPA対応実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, ProductListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
